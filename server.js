67e92657366b4cfb7f54e6d799b925c0ecca8965
K-Wiczling <wiczling.karol@gmail.com>
2023-02-25 11:51:43 +0100

    initial build of the server structure

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const n = (Number(req.query.id) * Number(req.query.di)).toString();
-    res.send(n);
-});
+app.post('/', (req,res) => {
+    res.send('Working...' );
+})
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('App is runninig on port 3000');
+})
+/*
+/ --> working...
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
